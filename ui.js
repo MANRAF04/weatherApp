040cca7871ec68809ff91b58b35dd832d590a4ad
manraf <stedion70@gmail.com>
2021-04-07 23:01:39 +0300

    Update ui.js

--- ui.js
+++ ui.js
@@ -27,6 +27,37 @@ class UI {
         
         
         `;
+    var counter = 2;
+    var day2,day3,day4,day5;
+    var iday2,iday3,iday4,iday5;
+    for (var i = 1; i <= 39; i++){
+        if (counter == 2) {
+            if (data.list[i].dt_txt.substring(0,10) != data.list[0].dt_txt.substring(0,10)){
+                day2 = data.list[i].dt_txt.substring(8,10)+'/'+data.list[i].dt_txt.substring(5,7);
+                iday2 = i;
+                counter++;
+            }
+        } else if (counter == 3) {
+            if (data.list[i].dt_txt.substring(0,10) != data.list[iday2].dt_txt.substring(0,10)){
+                day3 = data.list[i].dt_txt.substring(8,10)+'/'+data.list[i].dt_txt.substring(5,7)
+                iday3 = i;
+                counter++;
+            }
+        } else if (counter == 4) {
+            if (data.list[i].dt_txt.substring(0,10) != data.list[iday3].dt_txt.substring(0,10)){
+                day4 = data.list[i].dt_txt.substring(8,10)+'/'+data.list[i].dt_txt.substring(5,7)
+                iday4 = i;
+                counter++;
+            }
+        } else if (counter == 5) {
+            if (data.list[i].dt_txt.substring(0,10) != data.list[iday4].dt_txt.substring(0,10)){
+                day5 = data.list[i].dt_txt.substring(8,10)+'/'+data.list[i].dt_txt.substring(5,7)
+                iday5 = i;
+                counter++;
+            }
+        } else break;
+    }
+    console.log(iday2,iday3,iday4,iday5)
     let myChart = document.getElementById('myChart').getContext('2d');
     Chart.defaults.font.size = 18;
     Chart.defaults.font.family = 'Arial';
@@ -34,7 +65,7 @@ class UI {
         
         type: 'bar', 
         data: {
-            labels: [data.list[0].dt_txt.substring(8,10)+'/'+data.list[0].dt_txt.substring(5,7),data.list[8].dt_txt.substring(8,10)+'/'+data.list[8].dt_txt.substring(5,7),data.list[16].dt_txt.substring(8,10)+'/'+data.list[16].dt_txt.substring(5,7),data.list[24].dt_txt.substring(8,10)+'/'+data.list[24].dt_txt.substring(5,7),data.list[33].dt_txt.substring(8,10)+'/'+data.list[33].dt_txt.substring(5,7)],
+            labels: [data.list[0].dt_txt.substring(8,10)+'/'+data.list[0].dt_txt.substring(5,7),day2,day3,day4,day5],
             datasets: [{
                 label: 'Temperature',
                 data: [
